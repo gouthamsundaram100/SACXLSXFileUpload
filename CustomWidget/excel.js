584853907eeac4f2367d3b0390298695e732dba2
gouthamsundaram100 <49850003+gouthamsundaram100@users.noreply.github.com>
2020-11-20 13:29:01 -0500

    Update excel.js

--- CustomWidget/excel.js
+++ CustomWidget/excel.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "http://localhost/SAC/sacexcel/xlsx.js";
+            let xlsxjs = "https://raw.githubusercontent.com/gouthamsundaram100/SACXLSXFileUpload/master/CustomWidget/xlsx.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
